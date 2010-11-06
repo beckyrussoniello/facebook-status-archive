db7b88bda5d052658d677986b02189e2f0c4adeb
beckyrussoniello <rebecca.russoniello@gmail.com>
2010-11-06 12:11:12 -0700

    removed 'local version only' block

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,12 +2,6 @@ class WelcomeController < ApplicationController
 
 # INDEX
   def index
-    # LOCAL VERSION ONLY!!
-      session[:user_id] = 1
-    if session[:user_id]
-      @user = User.find(session[:user_id])
-      @apicall = Apicall.new
-    end
     if current_facebook_user
       current_facebook_user.fetch
       @user = User.find(session[:user_id])
